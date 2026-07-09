cec69f2751a133a30d51431bc4e46b4824c2a459
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-07-09 15:32:38 +0700

    void Input

---
+++ pertemuan2b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//Numeric nPanjang, nLebar, nKeliling
+int panjang, lebar, keliling;
+
+void input() {
+    //Display "Masukan Panjang : "
+    cout << "Masukan Panjang : ";
+    //Accept nPanjang 
+    cin >> panjang;
+    //Display "Masukan Lebar : "
+    cout << "Masukan Lebar : ";
+    //Accept nLebar
+    cin >> lebar;
+}
